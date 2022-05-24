72564e42ff9e81a88a401f90a728d188dd887901
NebojsaStojakovic <nebojsastojakovicla@gmail.com>
2022-05-24 13:31:13 +0200

    folder structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import AddContact from "./components/contacts/AddContact/AddContact";
+import ContactList from "./components/contacts/ContactList/ContactList";
+import EditContact from "./components/contacts/EditContact/EditContact";
+import ViewContact from "./components/contacts/ViewContact/ViewContact";
+import Navbar from "./components/Navbar/Navbar";
 const App = () => {
-  return <div>Learn React</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path={"/"} element={<Navigate to={<ContactList />} />} />
+        <Route path={"/contacts/list"} element={<ContactList />} />
+        <Route path={"/contacts/add"} element={<AddContact />} />
+        <Route path={"/contacts/view/:contactId"} element={<ViewContact />} />
+        <Route path={"/contacts/edit/:contactId"} element={<EditContact />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
